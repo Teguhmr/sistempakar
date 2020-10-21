c8b5213c1d7e9b2b3225c9ec8f0233c7561d34f2
Teguhmr <teguhmr16@gmail.com>
2020-10-21 07:43:55 +0700

    sistempakar

---
+++ konten/galeri.php
@@ -0,0 +1,16 @@
+<style>
+img {
+    padding:2px
+}
+#body1 {
+    margin-bottom:40px;
+    }
+</style>
+<div id="body1">
+<?php echo "<img src='images/pacman.png' width='100' height='100' />";
+echo "<img src='images/pisang.png' width='100' height='60' />";
+echo "<img src='images/roti.png' width='100' height='60' />";
+echo "<img src='images/jagung.jpg' width='100' height='60' />";
+echo "<img src='images/tempe.png' width='100' height='60' />";
+ echo "<img src='images/jeruk.png' width='100' height='70' />";?>
+ </div>
---
+++ konten/home.php
@@ -0,0 +1,10 @@
+<style>
+    #body {
+	font-family:sans-serif;
+    font-size: 12px;
+    margin-bottom:120px;
+    }
+</style>
+<div id="body">
+    <p>Halaman Utama Test</p>
+</div>
